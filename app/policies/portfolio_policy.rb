9d521aa708972a892bcd8f4ef16d0c1a157605b4
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-05-04 11:27:39 -0500

    add policy create for portfolio and refactor view for gallery

--- app/policies/portfolio_policy.rb
+++ app/policies/portfolio_policy.rb
@@ -7,6 +7,10 @@ class PortfolioPolicy < ApplicationPolicy
     record.public? || (record.user == user || user.role?(:admin) || user.is_his_job?(record)) 
   end
 
+  def create? 
+    user.present? && (record.user == user || user.role?(:admin))
+  end
+
   def gallery?
    true
   end
